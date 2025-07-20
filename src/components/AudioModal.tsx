d67aebe18eb4b6600475b34c7e891295d5892398
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-20 20:25:52 -0300

    feat: add upload audio

--- src/components/AudioModal.tsx
+++ src/components/AudioModal.tsx
@@ -1,3 +1,4 @@
+import { useMutation } from "@tanstack/react-query";
 import {
   AudioModule,
   RecordingPresets,
@@ -19,6 +20,7 @@ import {
 import { useEffect, useState } from "react";
 import { Alert, Modal, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { httpClient } from "../services/httpClient";
 import { colors } from "../styles/colors";
 import { cn } from "../utils/cn";
 import { Button } from "./Button";
@@ -34,6 +36,27 @@ export function AudioModal({ onClose, open }: IAudioModalProps) {
   const audioRecorder = useAudioRecorder(RecordingPresets.HIGH_QUALITY);
   const { isRecording } = useAudioRecorderState(audioRecorder);
   const player = useAudioPlayer(audioUri);
+
+  const { mutateAsync: createMeal } = useMutation({
+    mutationFn: async (uri: string) => {
+      const { data } = await httpClient.post("/meals", {
+        fileType: "audio/m4a",
+      });
+
+      const { uploadURL } = data;
+
+      const response = await fetch(uri);
+      const file = await response.blob();
+
+      await fetch(uploadURL, {
+        method: "PUT",
+        body: file,
+        headers: {
+          "Content-Type": file.type,
+        },
+      });
+    },
+  });
 
   useEffect(() => {
     (async () => {
@@ -165,7 +188,7 @@ export function AudioModal({ onClose, open }: IAudioModalProps) {
                   </Button>
                 )}
 
-                <Button size="icon">
+                <Button size="icon" onPress={() => createMeal(audioUri)}>
                   <CheckIcon size={20} color={colors.black[700]} />
                 </Button>
               </View>
