282377479f1a70a31d543157f1d69be902f0183a
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-20 11:35:49 -0300

    feat: add font host grotesk and config splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
+import {
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold,
+  useFonts,
+} from "@expo-google-fonts/host-grotesk";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+
+import { useEffect } from "react";
 import "./styles/global.css";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <View className="bg-lime-600 text-slate-200 flex-1 justify-center items-center">
       <Text className="text-black-700">foodiary!</Text>
