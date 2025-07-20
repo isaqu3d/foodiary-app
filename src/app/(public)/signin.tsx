61f0893697ba60c0dec9901a6c26e26e43daa835
Isaque de Sousa <isaque.error404@gmail.com>
2025-07-20 17:24:35 -0300

    refactor: add auth layout

--- src/app/(public)/signin.tsx
+++ src/app/(public)/signin.tsx
@@ -1,9 +1,87 @@
-import { Text, View } from "react-native";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { router } from "expo-router";
+import { ArrowLeftIcon } from "lucide-react-native";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { View } from "react-native";
+import z from "zod";
+
+import { Button } from "../../components/Button";
+
+import { AuthLayout } from "../../components/AuthLayout";
+import { Input } from "../../components/Input";
+import { colors } from "../../styles/colors";
+
+const schema = z.object({
+  email: z.email("Informe um e-mail válido"),
+  password: z.string().min(8, "Deve conter pelo menos 8 caracteres"),
+});
 
 export default function SignIn() {
+  const form = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = form.handleSubmit((formData) => {
+    console.log(JSON.stringify(formData, null, 2));
+  });
+
   return (
-    <View>
-      <Text>Entrar...</Text>
-    </View>
+    <AuthLayout
+      icon="👤"
+      title="Entre em sua conta"
+      subtitle="Acesse sua conta para continuar"
+    >
+      <View className="justify-between flex-1">
+        <View className="gap-6">
+          <Controller
+            control={form.control}
+            name="email"
+            render={({ field, fieldState }) => (
+              <Input
+                label="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                autoComplete="email"
+                value={field.value}
+                onChangeText={field.onChange}
+                error={fieldState.error?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={form.control}
+            name="password"
+            render={({ field, fieldState }) => (
+              <Input
+                label="Senha"
+                autoCapitalize="none"
+                autoCorrect={false}
+                autoComplete="password"
+                secureTextEntry
+                value={field.value}
+                onChangeText={field.onChange}
+                error={fieldState.error?.message}
+              />
+            )}
+          />
+        </View>
+
+        <View className="flex-row gap-6">
+          <Button onPress={router.back} size="icon" color="gray">
+            <ArrowLeftIcon size={20} color={colors.black[700]} />
+          </Button>
+          <Button className="flex-1" onPress={handleSubmit}>
+            Entrar
+          </Button>
+        </View>
+      </View>
+    </AuthLayout>
   );
 }
